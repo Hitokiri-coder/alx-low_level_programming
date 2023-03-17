71ae167d59583c08b8c6f14359186251dd516cca
Hitokiri-coder <hitotoabdodo@gmail.com>
2023-03-17 04:28:51 +0200

    RTFM

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Description: the entry point of the program
+ * Return: returns 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
