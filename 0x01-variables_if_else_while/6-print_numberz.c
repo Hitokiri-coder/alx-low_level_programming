671b5b77546288c992cf7d209c3dd041b7b2b9d7
Hitokiri-coder <hitotoabdodo@gmail.com>
2023-03-18 01:00:04 +0200

    RTFM

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the main func
+ * Description: the entry point of the program
+ * Return: returns 0 and close
+ */
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
