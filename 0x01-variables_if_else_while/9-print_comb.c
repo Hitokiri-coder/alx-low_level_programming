d819990d3b071da0223fd057fe1f3e3593084fea
Hitokiri-coder <hitotoabdodo@gmail.com>
2023-03-18 01:50:24 +0200

    RTFM

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the main func
+ * Description: the entry point of the program
+ * Return: returns 0 and close
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num < 57)
+	{
+		putchar(num++);
+		if (num == 58)
+			putchar(44);
+			putchar(32);
+	}
+	return (0);
+}
