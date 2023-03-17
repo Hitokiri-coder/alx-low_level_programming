3bc0bb861aef4b91a82a6401de369002b49dd375
Hitokiri-coder <hitotoabdodo@gmail.com>
2023-03-17 17:41:35 +0200

    RTFM

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ * Description: the main function
+ * Return: return 1 according to the alx project
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
