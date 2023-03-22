5ea8943aa5b5734af3a8b55dc4e1b7620b12dbb3
Hitokiri-coder <hitotoabdodo@gmail.com>
2023-03-22 19:23:00 +0200

    RTFM

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the main func of the project
+ * Description: the entry point of the file
+ * Return: return 0 and close
+ */
+int main(void)
+{
+	char* msg;
+	msg = "_puchar";
+	printf("%s\n", msg);
+	return (0);
+}
