59a51ddf80cde1ed424d9ac624045bf7a7cfec0c
Hitokiri-coder <hitotoabdodo@gmail.com>
2023-03-24 15:58:51 +0200

    e

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 
-char a = 'a';
+/**
+ * print_alphabet: Always return an error.
+ */
 void print_alphabet(void)
 {
+	char a = 'a';
         while (a <= 'z')
 	{
 		_putchar(a++);
