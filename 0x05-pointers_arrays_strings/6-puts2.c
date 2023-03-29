a225ea61d45f23e88b7ef73df8506cd598787afc
Hitokiri-coder <hitotoabdodo@gmail.com>
2023-03-29 02:20:58 +0200

    GEJ

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,29 @@ void puts2(char *str)
 {
 	int counter = 0;
 
-	while (str[counter] != '\0')
+	while (counter < _strlen(str))
 	{
 		_putchar(str[counter]);
 		counter += 2;
 	}
 	_putchar('\n');
+}
+
+/**
+ * _strlen - calculate the length of a given string
+ * @s: fixed var that contains the passed string.
+ *
+ * Return: returns the number of the given characters
+ */
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	while (s)
+	{
+		if (s[counter] == '\0')
+			break;
+		counter++;
+	}
+	return (counter);
 }
