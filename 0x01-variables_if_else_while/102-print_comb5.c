ed142dbf598391eaf29abc80552022823355ca15
Hitokiri-coder <hitotoabdodo@gmail.com>
2023-04-01 14:10:21 +0200

    Rtm

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,51 +8,24 @@
 int main(void)
 {
 	int num_1 = 0;
-	int num_2 = 0;
-	int num_3 = num_1;
-	int num_4 = num_2 + 1;
+	int num_2;
 
-	while (num_1 < 9 || num_2 < 9)
+	while (num_1 <= 98)
 	{
-		putchar('0' + num_1);
-		putchar('0' + num_2);
-		putchar(' ');
-		putchar('0' + num_3);
-		putchar('0' + num_4);
-		if (!(num_1 == 9 && num_2 == 8 && num_3 == 9 && num_4 == 9))
+		num_2 = num_1 + 1;
+		while (num_2 <= 99)
 		{
-			putchar(',');
+			putchar('0' + (num_1 - num_1 % 10) / 10);
+			putchar('0' + (num_1 % 10));
 			putchar(' ');
+			putchar('0' + (num_2 - num_2 % 10) / 10);
+			putchar('0' + (num_2 % 10));
+			if (!(num_1 == 98 && num_2 == 99))
+				putchar(',');
+			putchar(' ');
+			num_2 += 1;
 		}
-		if (num_2 == 9 && num_3 == 9 && num_4 == 9)
-		{
-			num_1 += 1;
-			num_2 = 0;
-			num_3 = num_1;
-			num_4 = num_2 + 1;
-		}
-		else if (num_3 == 9 && num_4 == 9)
-		{
-			if (num_2 == 8)
-			{
-				num_2 += 1;
-				num_3 = num_1 + 1;
-				num_4 = 0;
-			}
-			else
-			{
-				num_2 += 1;
-				num_3 = num_1;
-				num_4 = num_2 + 1;
-			}
-		}
-		else if (num_4 == 9)
-		{
-			num_3 += 1;
-			num_4 = 0;
-		}
-		else
-			num_4 += 1;
+		num_1 += 1;
 	}
 	putchar('\n');
 	return (0);
