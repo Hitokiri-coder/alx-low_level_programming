30b91c26cb449130a48283ff12448ef87b9b762e
Hitokiri-coder <hitotoabdodo@gmail.com>
2023-04-02 12:00:35 +0000

    RTFM

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints numbers from n to 98 including the n and 98 numbers.
+ * @n: contains: the start number.
+ */
+void print_to_98(int n)
+{
+	while (n < 99)
+	{
+		if (n != 98)
+			printf("%d, ", n);
+		else
+			printf("%d", n);
+		n += 1;
+	}
+}
