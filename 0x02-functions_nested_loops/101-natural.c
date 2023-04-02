5240f0f868c5ecda52928163189c09c77a6442e3
Hitokiri-coder <hitotoabdodo@gmail.com>
2023-04-02 18:29:22 +0000

    RTFM

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -10,15 +10,16 @@ int main(void)
 	int n = 0;
 	int sum = 0;
 
-	while (n * 3 < 1024)
+	while (n * 3 < 1000)
 	{
 		sum += n * 3;
 		n += 1;
 	}
 	n = 0;
-	while (n * 5 < 1024)
+	while (n * 5 < 1000)
 	{
-		sum += n * 5;
+		if ((n * 5) % 15 != 0)
+			sum += n * 5;
 		n += 1;
 	}
 	printf("%d\n", sum);
